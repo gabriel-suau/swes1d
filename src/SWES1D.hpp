60c20158d830128eee785a3af4f579f74120698f
Gabriel Suau <gabrielsuau2@gmail.com>
2022-05-29 19:02:45 +0200

    [LICENSE] Add license info at the beginning of each source file

--- src/SWES1D.hpp
+++ src/SWES1D.hpp
@@ -1,3 +1,28 @@
+/**
+ * \file SWES1D.hpp
+ * \author Gabriel Suau <gabrielsuau2@gmail.com> (2022)
+ * \version 1.0.0
+ * \date 2022-05-29
+ *
+ * \brief Wrapper include file of the library
+ *
+ * \copyright SWES1D : Monodimensional Shallow Water Equations Solvers
+ * Copyright (C) 2022 Gabriel Suau
+ *
+ * \copyright This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * \copyright This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * \copyright You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef SWES1D_HPP
 #define SWES1D_HPP
 
